d85f5e47de126c4b3dc3b29da28a6b5ae56aca54
LogisticBravo <jay.jbradley@gmail.com>
2021-09-27 19:40:32 +0000

    Add responsive css. Add home.js

---
+++ home/static/home/js/home.js
@@ -0,0 +1,10 @@
+var logo = document.getElementById("flick")
+
+logo.onmouseover = function(){
+this.classList.remove("flicker");
+this.classList.remove("no-flicker")
+this.classList.add("flickering");}
+
+logo.onmouseout = function(){
+this.classList.remove("flickering");
+this.classList.add("no-flicker");}
