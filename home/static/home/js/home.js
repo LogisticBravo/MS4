fc1e8f90ed12e9f7c8e9fc2200e50e70b5656b27
LogisticBravo <jay.jbradley@gmail.com>
2021-10-17 20:11:54 +0000

    redesign index.html. restyle index.html

--- home/static/home/js/home.js
+++ home/static/home/js/home.js
@@ -8,3 +8,13 @@ this.classList.add("flickering");}
 logo.onmouseout = function(){
 this.classList.remove("flickering");
 this.classList.add("no-flicker");}
+
+var shopButton = document.getElementById("shop")
+
+shopButton.onmouseover = function(){
+    this.classList.add("flickering")
+}
+
+shopButton.onmouseleave = function(){
+    this.classList.remove("flickering")
+}
