c20380704d9eaf04a65c28ca584ff6b9d80e8738
LogisticBravo <jay.jbradley@gmail.com>
2021-10-28 21:52:15 +0000

    Add meta options for blogposts and contactus. Create products app. Create category and product model.

--- MS4/settings.py
+++ MS4/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
     'home',
     'blog',
     'contact',
+    'products',
 
     # Other
     'crispy_forms',
---
+++ products/models.py
@@ -0,0 +1,50 @@
+from django.db import models
+
+
+class Category(models.Model):
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
+class Product(models.Model):
+    category = models.ForeignKey('Category', null=True, blank=True,
+                                 on_delete=models.SET_NULL)
+    brand = models.CharField(max_length=254)
+    model_name = models.CharField(max_length=254)
+    alternate_names = models.CharField(max_length=254, null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+    launched = models.CharField(max_length=254)
+    console_family = models.CharField(max_length=254)
+    console_type = models.CharField(max_length=254)
+    input_method = models.CharField(max_length=254)
+    hard_disk = models.CharField(max_length=254)
+    ram = models.CharField(max_length=254)
+    processor = models.CharField(max_length=254)
+    weight = models.CharField(max_length=254, null=True, blank=True)
+    battery_type = models.CharField(max_length=254, null=True, blank=True)
+    screen_size = models.CharField(max_length=254, null=True, blank=True)
+    graphics = models.BooleanField(default=False, null=True, blank=True)
+    hdmi = models.BooleanField(default=False, null=True, blank=True)
+    usb = models.BooleanField(default=False, null=True, blank=True)
+    has_wifi = models.BooleanField(default=False, null=True, blank=True)
+    ethernet = models.BooleanField(default=False, null=True, blank=True)
+    memory_card = models.BooleanField(default=False, null=True, blank=True)
+    av_digital_output = models.BooleanField(default=False, null=True,
+                                            blank=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True,
+                                 blank=True)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.model_name
