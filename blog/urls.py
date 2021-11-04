1a696604f4788fcf1fe024f3e3949078e15ed979
LogisticBravo <jay.jbradley@gmail.com>
2021-11-04 19:15:21 +0000

    Secure the views

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
+from django.contrib.auth.decorators import login_required
 from . import views
 
 urlpatterns = [
     path('', views.BlogPostView.as_view(), name='blog'),
     path('add/', views.CreatePostView.as_view(), name='add_post'),
     path('posts/<int:pk>', views.FullPostView.as_view(), name='post'),
-    path('posts/edit/<int:pk>', views.EditPostView.as_view(), name='edit_post'),
-    path('posts/<int:pk>/delete', views.DeletePostView.as_view(), name='delete_post'),
+    path('posts/edit/<int:pk>', login_required(views.EditPostView.as_view()), name='edit_post'),
+    path('posts/<int:pk>/delete', login_required(views.DeletePostView.as_view()), name='delete_post'),
 ]
